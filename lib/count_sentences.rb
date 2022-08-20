97555877f7b6193ec7424908c18ce9ad768e4e17
wandiamugo <wwandia173@gmail.com>
2022-08-20 10:25:43 +0300

    commit tree ba9945e48c0147029200ba975755d5ffa3f50a55

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_width?("?")
   end
 
   def exclamation?
-
+    self.end_width?("!")
   end
 
   def count_sentences
-
-  end
+    self.split(/\.|\?|!/).filter {|sentence| !sentence.empty?}.size
+    end
 end
+"Hi.my name is sophie".sentence?
+"hello?".question
+"Haiya!".exclamation?
+"returns the number of sentences".count_sentences"
+"This is a string! It has three sentences. Right?".count_sentences
